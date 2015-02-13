798b88f0703cd7de5ffac4b3f089151c13f4e228
unknown <stanislav.yuzvinsky@sw-0050.teknavo.local>
2015-02-13 12:51:06 +0300

    Change wrong naming adpator->proxy

---
+++ desiredimpl/01-basic-scenario/timer_proxy.cpp
@@ -0,0 +1,21 @@
+#include <timer_proxy.hpp>
+
+namespace dboost
+{
+
+timer_proxy::timer_proxy()
+{}
+
+timer_proxy::~timer_proxy() {}
+
+long timer_proxy::add_timer(const long& timeout_ms)
+{
+}
+
+void timer_proxy::remove_timer(const long& timer_id)
+{
+}
+
+
+} // namespace dboost
+
