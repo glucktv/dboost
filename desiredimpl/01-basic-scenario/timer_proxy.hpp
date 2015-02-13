5b0310f49a525f58b33f48aaf02aa4c7d5f5e3b3
unknown <stanislav.yuzvinsky@sw-0050.teknavo.local>
2015-02-13 12:51:06 +0300

    Change wrong naming adaptor->proxy

--- desiredimpl/01-basic-scenario/timer_proxy.hpp
+++ desiredimpl/01-basic-scenario/timer_proxy.hpp
@@ -6,10 +6,10 @@
 namespace dboost
 {
 
-class timer_adaptor: public timer
+class timer_proxy: public timer
 {
 public:
-  virtual ~timer_adaptor() {}
+  virtual ~timer_proxy() {}
   virtual long add_timer(const long& timeout_ms); // override;
   virtual void remove_timer(const long& timer_id); // override;
 };
